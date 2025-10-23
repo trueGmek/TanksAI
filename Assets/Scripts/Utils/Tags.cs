c2f4ac5ea544ea5cc882d6cb3945c40f1522a958
gmek <gmek0204@gmail.com>
2025-10-23 14:40:42 +0200

    Formatted a file

--- Assets/Scripts/Utils/Tags.cs
+++ Assets/Scripts/Utils/Tags.cs
@@ -1,18 +1,18 @@
 namespace Utils
 {
-    public static class Tags
-    {
-        public const string AGENT = "Agent";
-        public const string SPAWNER = "Spawner";
-        public const string MANUAL_TESTER = "ManualTester";
-        public const string CANON = "Canon";
-        public const string DAMAGE_APPLIER = "DamageApplier";
-        public const string DAMAGE_PROCESSOR = "DamageProcessor";
-        public const string LUA = "Lua";
-        public const string LUA_MANAGER = "LuaManager";
+  public static class Tags
+  {
+    public const string AGENT = "Agent";
+    public const string SPAWNER = "Spawner";
+    public const string MANUAL_TESTER = "ManualTester";
+    public const string CANON = "Canon";
+    public const string DAMAGE_APPLIER = "DamageApplier";
+    public const string DAMAGE_PROCESSOR = "DamageProcessor";
+    public const string LUA = "Lua";
+    public const string LUA_MANAGER = "LuaManager";
 
 #if UNITY_EDITOR
-        public const string LUA_CODE_GEN = "LuaCodeGen";
+    public const string LUA_CODE_GEN = "LuaCodeGen";
 #endif
-    }
+  }
 }
